f92bf9ad5da8b1d8671dd6b6836b6178c1af70d2
Scott Greenlay <scott@greenlay.net>
2020-12-18 22:23:06 -0800

    Day 19 - Part 1

--- Sources/Day19/main.swift
+++ Sources/Day19/main.swift
@@ -2,33 +2,10 @@ import Foundation
 import Utils
 
 enum Rule {
-    case MatchRules(Int, [[Int]])
-    case MatchCharacter(Int, Character)
+    case MatchOneOf([[Int]])
+    case MatchCharacter(Character)
     
-    public func isMatch(_ input: String, index: inout Int, rules: [Rule]) -> Bool {
-        switch self {
-            case .MatchRules(_, let toMatch):
-                for match in toMatch {
-                    var i = index
-                    let matches = match.reduce(true, {
-                        $0 && rules[$1].isMatch(input, index: &i, rules: rules)
-                    })
-                    if matches {
-                        index = i
-                        return true
-                    }
-                }
-                return false
-            case .MatchCharacter(_, let ch):
-                if input.characterAt(offset: index) == ch {
-                    index += 1
-                    return true
-                }
-                return false
-        }
-    }
-    
-    public static func parse(_ input: String) -> Rule {
+    public static func parse(_ input: String) -> (Int, Rule) {
         let scanner = Scanner(string: input)
         
         let id = scanner.scanInt()!
@@ -36,7 +13,7 @@ enum Rule {
         
         if scanner.scanString("\"") != nil {
             let ch = scanner.scanCharacter()!
-            return .MatchCharacter(id, ch)
+            return (id, .MatchCharacter(ch))
         } else {
             let rules = input
                 .suffix(from: scanner.currentIndex)
@@ -46,20 +23,21 @@ enum Rule {
                       .components(separatedBy: " ")
                       .map({ $0.asInt()! })
                 })
-            return .MatchRules(id, rules)
+            return (id, .MatchOneOf(rules))
         }
     }
 }
 
-func parseInput(_ input: String) -> ([Rule], [String]) {
+func parseInput(_ input: String) -> ([Int: Rule], [String]) {
     let lines = input
         .trimmingCharacters(in: .whitespacesAndNewlines)
         .components(separatedBy: "\n")
     
     var i = 0
-    var rules = [Rule]()
+    var rules = [Int: Rule]()
     while i < lines.count && lines[i] != "" {
-        rules.append(Rule.parse(lines[i]))
+        let (id, rule) = Rule.parse(lines[i])
+        rules[id] = rule
         i += 1
     }
     i += 1
@@ -68,11 +46,35 @@ func parseInput(_ input: String) -> ([Rule], [String]) {
     return (rules, cases)
 }
 
+func isMatch(_ input: inout String, rule: Int, rules: [Int: Rule]) -> Bool {
+    if input.count == 0 {
+        return false
+    }
+    
+    switch rules[rule]! {
+        case .MatchCharacter(let ch):
+            if input.characterAt(offset: 0) == ch {
+                input.remove(at: input.indexAt(offset: 0))
+                return true
+            }
+            return false
+        case .MatchOneOf(let possibilities):
+            for match in possibilities {
+                var nextInput = input
+                if match.first(where: { !isMatch(&nextInput, rule: $0, rules: rules) }) == nil {
+                    input = nextInput
+                    return true
+                }
+            }
+            return false
+    }
+}
+
 func part1(_ input: String) -> Int {
     let (rules, cases) = parseInput(input)
-    let valid = cases.filter({
-        var index = 0
-        return rules[0].isMatch($0, index: &index, rules: rules) && (index == $0.count)
+    let valid = cases.filter({ input in
+        var temp = input
+        return isMatch(&temp, rule: 0, rules: rules) && temp.count == 0
     })
     return valid.count
 }
