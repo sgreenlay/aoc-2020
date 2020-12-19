43bfd87483eacec431a1837cf40bdaa736532a12
Scott Greenlay <scott@greenlay.net>
2020-12-18 22:36:27 -0800

    Day 19 - Part 2

--- Sources/Day19/main.swift
+++ Sources/Day19/main.swift
@@ -46,36 +46,41 @@ func parseInput(_ input: String) -> ([Int: Rule], [String]) {
     return (rules, cases)
 }
 
-func isMatch(_ input: inout String, rule: Int, rules: [Int: Rule]) -> Bool {
-    if input.count == 0 {
-        return false
-    }
-    
+func possibleMatches(_ input: String, rule: Int, rules: [Int: Rule]) -> [String] {
     switch rules[rule]! {
         case .MatchCharacter(let ch):
             if input.characterAt(offset: 0) == ch {
-                input.remove(at: input.indexAt(offset: 0))
-                return true
+                return [String(input.suffix(from: input.indexAt(offset: 1)))]
             }
-            return false
+            return []
         case .MatchOneOf(let possibilities):
-            for match in possibilities {
-                var nextInput = input
-                if match.first(where: { !isMatch(&nextInput, rule: $0, rules: rules) }) == nil {
-                    input = nextInput
-                    return true
+            var possible = [String]()
+            for toMatch in possibilities {
+                var starts = [input]
+                for rule in toMatch {
+                    var nextStarts = [String]()
+                    for start in starts {
+                        if start == "" {
+                            break
+                        }
+                        nextStarts.append(contentsOf: possibleMatches(start, rule: rule, rules: rules))
+                    }
+                    starts = nextStarts
                 }
+                possible.append(contentsOf: starts)
             }
-            return false
+            return possible
     }
+}
+
+func isMatch(_ input: String, rule: Int, rules: [Int: Rule]) -> Bool {
+    let matches = possibleMatches(input, rule: rule, rules: rules)
+    return matches.first(where: { $0 == "" }) != nil
 }
 
 func part1(_ input: String) -> Int {
     let (rules, cases) = parseInput(input)
-    let valid = cases.filter({ input in
-        var temp = input
-        return isMatch(&temp, rule: 0, rules: rules) && temp.count == 0
-    })
+    let valid = cases.filter({ isMatch($0, rule: 0, rules: rules) })
     return valid.count
 }
 
@@ -93,8 +98,122 @@ abbbab
 aaabbb
 aaaabbb
 """) == 2, "part1")
+
+try! test(part1("""
+42: 9 14 | 10 1
+9: 14 27 | 1 26
+10: 23 14 | 28 1
+1: "a"
+11: 42 31
+5: 1 14 | 15 1
+19: 14 1 | 14 14
+12: 24 14 | 19 1
+16: 15 1 | 14 14
+31: 14 17 | 1 13
+6: 14 14 | 1 14
+2: 1 24 | 14 4
+0: 8 11
+13: 14 3 | 1 12
+15: 1 | 14
+17: 14 2 | 1 7
+23: 25 1 | 22 14
+28: 16 1
+4: 1 1
+20: 14 14 | 1 15
+3: 5 14 | 16 1
+27: 1 6 | 14 18
+14: "b"
+21: 14 1 | 1 14
+25: 1 1 | 1 14
+22: 14 14
+8: 42
+26: 14 22 | 1 20
+18: 15 15
+7: 14 5 | 1 21
+24: 14 1
+
+abbbbbabbbaaaababbaabbbbabababbbabbbbbbabaaaa
+bbabbbbaabaabba
+babbbbaabbbbbabbbbbbaabaaabaaa
+aaabbbbbbaaaabaababaabababbabaaabbababababaaa
+bbbbbbbaaaabbbbaaabbabaaa
+bbbababbbbaaaaaaaabbababaaababaabab
+ababaaaaaabaaab
+ababaaaaabbbaba
+baabbaaaabbaaaababbaababb
+abbbbabbbbaaaababbbbbbaaaababb
+aaaaabbaabaaaaababaa
+aaaabbaaaabbaaa
+aaaabbaabbaaaaaaabbbabbbaaabbaabaaa
+babaaabbbaaabaababbaabababaaab
+aabbbbbaabbbaaaaaabbbbbababaaaaabbaaabba
+""") == 3, "part1")
+
+func part2(_ input: String) -> Int {
+    var (rules, cases) = parseInput(input)
+    
+    /*
+     8: 42 | 42 8
+     11: 42 31 | 42 11 31
+     */
+    rules[8] = .MatchOneOf([[42], [42, 8]])
+    rules[11] = .MatchOneOf([[42, 31], [42, 11, 31]])
+    
+    let valid = cases.filter({ isMatch($0, rule: 0, rules: rules) })
+    return valid.count
+}
+
+try! test(part2("""
+42: 9 14 | 10 1
+9: 14 27 | 1 26
+10: 23 14 | 28 1
+1: "a"
+11: 42 31
+5: 1 14 | 15 1
+19: 14 1 | 14 14
+12: 24 14 | 19 1
+16: 15 1 | 14 14
+31: 14 17 | 1 13
+6: 14 14 | 1 14
+2: 1 24 | 14 4
+0: 8 11
+13: 14 3 | 1 12
+15: 1 | 14
+17: 14 2 | 1 7
+23: 25 1 | 22 14
+28: 16 1
+4: 1 1
+20: 14 14 | 1 15
+3: 5 14 | 16 1
+27: 1 6 | 14 18
+14: "b"
+21: 14 1 | 1 14
+25: 1 1 | 1 14
+22: 14 14
+8: 42
+26: 14 22 | 1 20
+18: 15 15
+7: 14 5 | 1 21
+24: 14 1
+
+abbbbbabbbaaaababbaabbbbabababbbabbbbbbabaaaa
+bbabbbbaabaabba
+babbbbaabbbbbabbbbbbaabaaabaaa
+aaabbbbbbaaaabaababaabababbabaaabbababababaaa
+bbbbbbbaaaabbbbaaabbabaaa
+bbbababbbbaaaaaaaabbababaaababaabab
+ababaaaaaabaaab
+ababaaaaabbbaba
+baabbaaaabbaaaababbaababb
+abbbbabbbbaaaababbbbbbaaaababb
+aaaaabbaabaaaaababaa
+aaaabbaaaabbaaa
+aaaabbaabbaaaaaaabbbabbbaaabbaabaaa
+babaaabbbaaabaababbaabababaaab
+aabbbbbaabbbaaaaaabbbbbababaaaaabbaaabba
+""") == 12, "part2")
  
 let input = try! String(contentsOfFile: "input/day19.txt", encoding: String.Encoding.utf8)
 
 print("\(part1(input))")
-//print("\(part2(input))")
+print("\(part2(input))")
